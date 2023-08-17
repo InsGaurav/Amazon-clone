39ac46a4fc6fa8cf25cba6dd81475ab087c4f17d
ShyanCS <shyan@cvs.du.ac.in>
2023-08-17 10:38:06 +0530

    Generated results HTML with JS

---
+++ scripts/products.js
@@ -0,0 +1,242 @@
+export const items = [{
+    image: '../images/products/watch1.webp',
+    name: 'Fire-Boltt Phoenix Pro 1.39" Bluetooth Calling Smartwatch, AI Voice Assistant, Metal Body with 120+ Sports Modes, SpO2, Heart Rate Monitoring (Black)',
+    rating: 43,
+    price: 1599,
+    mrp: 11999,
+    discount: 87,
+    ltd: false,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch2.webp',
+    name: 'Fire-Boltt Phoenix Pro 1.39" Bluetooth Calling Smartwatch, AI Voice Assistant, Metal Body with 120+ Sports Modes, SpO2, Heart Rate Monitoring (Black)',
+    rating: 40,
+    price: 1399,
+    mrp: 2199,
+    discount: 36,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch3.webp',
+    name: 'Fire-Boltt Visionary 1.78" AMOLED Bluetooth Calling Smartwatch with 368 * 448 Pixel Resolution, Rotating Crown & 60Hz Refresh Rate 100+ Sports Mode, TWS Connection, Voice Assistance (Black)',
+    rating: 45,
+    price: 2799,
+    mrp: 16999,
+    discount: 84,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch4.webp',
+    name: 'Noise Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate Monitor, Sleep Tracking (Jet Black)',
+    rating: 40,
+    price: 1799,
+    mrp: 4999,
+    discount: 64,
+    ltd: false,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch5.webp',
+    name: 'Noise ColorFit Pulse Smartwatch with 3.55cm (1.4") Full Touch Display, SpO2, Heart Rate, Sleep Monitors & 10-Day Battery - Jet Black',
+    rating: 40,
+    price: 1199,
+    mrp: 4999,
+    discount: 76,
+    ltd: false,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch6.webp',
+    name: 'Fire-Boltt Ninja 3 1.83" Display Smartwatch Full Touch with 100+ Sports Modes with IP68, Sp02 Tracking, Over 100 Cloud Based Watch Faces (Black)',
+    rating: 40,
+    price: 1099,
+    mrp: 9999,
+    discount: 89,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch7.webp',
+    name: 'PTron Force X12N 1.85" Full Touch HD Display Bluetooth Calling Smartwatch, Functional Crown, 580 NITS Brightness, HR, SpO2, Watch Faces, Inbuilt Games, 5 Days Battery Life & IP68 Waterproof (Black)',
+    rating: 34,
+    price: 1549,
+    mrp: 4899,
+    discount: 68,
+    ltd: true,
+    dod: false,
+    ed: false
+},{
+    image: '../images/products/watch8.webp',
+    name: 'Fire-Boltt Ring 3 Smart Watch 1.8 Biggest Display with Advanced Bluetooth Calling Chip, Voice Assistance,118 Sports Modes, in Built Calculator & Games, SpO2, Heart Rate Monitoring (Black)',
+    rating: 41,
+    price: 1499,
+    mrp: 9999,
+    discount: 85,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch9.webp',
+    name: "Fire-Boltt India's No 1 Smartwatch Brand Talk 2 Bluetooth Calling Smartwatch with Dual Button, Hands On Voice Assistance, 120 Sports Modes, in Built Mic & Speaker with IP68 Rating (Black)",
+    rating: 43,
+    price: 1499,
+    mrp: 9999,
+    discount: 85,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch10.webp',
+    name: 'Fire-Boltt Eterno 1.99" Largest Display, Bluetooth Calling Smartwatch with 1184mm Display Area, AI Voice Assistant, 120+ Sports Modes (Yoga) with Smart Split Screen to Toggle between functions (Black)',
+    rating: 43,
+    price: 1699,
+    mrp: 18999,
+    discount: 91,
+    ltd: false,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch11.webp',
+    name: 'Fire-Boltt Ninja Call Pro Plus 1.83" Smart Watch with Bluetooth Calling, AI Voice Assistance, 100 Sports Modes IP67 Rating, 240 * 280 Pixel High Resolution (Grey)',
+    rating: 42,
+    price: 1499,
+    mrp: 19999,
+    discount: 93,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch12.webp',
+    name: 'Fire-Boltt Ninja 3 Smartwatch Full Touch 1.69 & 60 Sports Modes with IP68, Sp02 Tracking, Over 100 Cloud based watch faces (Black)',
+    rating: 42,
+    price: 1099,
+    mrp: 7999,
+    discount: 86,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch13.webp',
+    name: 'Noise Twist Round Dial Smart Watch with Bluetooth Calling, 1.38" TFT Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate Monitor, Sleep Tracking (Silver Grey)',
+    rating: 40,
+    price: 1849,
+    mrp: 4999,
+    discount: 63,
+    ltd: false,
+    dod: true,
+    ed: true
+},{
+    image: '../images/products/watch14.webp',
+    name: 'Noise ColorFit Ultra Buzz Bluetooth Calling Smart Watch with 1.75" HD Display, 320x385 px Resolution, 100 Sports Modes, Stock Market Info Smartwatch for Men & Women (Charcoal Black)',
+    rating: 41,
+    price: 1799,
+    mrp: 5999,
+    discount: 70,
+    ltd: false,
+    dod: true,
+    ed: true
+},{
+    image: '../images/products/watch15.webp',
+    name: 'Noise Pulse Go Buzz Smart Watch with Advanced Bluetooth Calling, 1.69" TFT Display, SpO2, 100 Sports Mode with Auto Detection, Upto 7 Days Battery (2 Days with Heavy Calling) - Midnight Blue',
+    rating: 41,
+    price: 1599,
+    mrp: 4999,
+    discount: 68,
+    ltd: false,
+    dod: false,
+    ed: false
+},{
+    image: '../images/products/watch16.webp',
+    name: 'Fire-Boltt Phoenix Smart Watch with Bluetooth Calling 1.3",120+ Sports Modes, 240 * 240 PX High Res with SpO2, Heart Rate Monitoring & IP67 Rating (Gold Pink)',
+    rating: 42,
+    price: 1399,
+    mrp: 9999,
+    discount: 86,
+    ltd: true,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch17.webp',
+    name: 'pTron Force X12S Bluetooth Calling Smartwatch, 1.85" Full Touch HD Display, Functional Crown, Real Heart Rate Monitor, SpO2, WatchFaces, 5 Days Battery Life, Fitness Trackers & IP68 Waterproof (Black)',
+    rating: 32,
+    price: 1099,
+    mrp: 4599,
+    discount: 76,
+    ltd: true,
+    dod: false,
+    ed: false
+},{
+    image: '../images/products/watch18.webp',
+    name: 'TAGG Verve NEO Smartwatch 1.69’’ HD Display | 60+ Sports Modes | 10 Days Battery | 150+ Maximum Watch Face Library | Waterproof | 24 * 7 HeartRate & Blood Oxygen Tracking | Games & Calculator | Grey',
+    rating: 39,
+    price: 999,
+    mrp: 3999,
+    discount: 75,
+    ltd: true,
+    dod: true,
+    ed: false
+},{
+    image: '../images/products/watch19.webp',
+    name: 'Noise Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate Monitor, Sleep Tracking (Midnight Blue)',
+    rating: 40,
+    price: 1849,
+    mrp: 4999,
+    discount: 63,
+    ltd: false,
+    dod: true,
+    ed: true
+},{
+    image: '../images/products/watch20.webp',
+    name: 'NoiseFit Force Rugged Round Dial Bluetooth Calling Smart Watch with 1.32" HD Screen, Functional Crown, 550 NITS, 7 Days Battery, AI Voice Assistance, 200+ Watch Faces, Heart Rate Tracker- (Jet Black)',
+    rating: 39,
+    price: 2799,
+    mrp: 5999,
+    discount: 53,
+    ltd: false,
+    dod: false,
+    ed: true
+},{
+    image: '../images/products/watch21.webp',
+    name: 'Noise Pulse 2 Max 1.85" Display, Bluetooth Calling Smart Watch, 10 Days Battery, 550 NITS Brightness, Smart DND, 100 Sports Modes, Smartwatch for Men and Women (Jade Green)',
+    rating: 40,
+    price: 1699,
+    mrp: 5999,
+    discount: 72,
+    ltd: false,
+    dod: true,
+    ed: true
+
+},{
+    image: '../images/products/watch22.webp',
+    name: 'Noise Pulse 2 Max 1.85" Display, Bluetooth Calling Smart Watch, 10 Days Battery, 550 NITS Brightness, Smart DND, 100 Sports Modes, Smartwatch for Men and Women (Jet Black)',
+    rating: 40,
+    price: 1699,
+    mrp: 5999,
+    discount: 72,
+    ltd: false,
+    dod: true,
+    ed: true
+},{
+    image: '../images/products/watch23.webp',
+    name: 'Fire-Boltt Rage Full Touch 1.28” Display & 60 Sports Modes with IP68 Rating Smartwatch, Sp02 Tracking, Over 100 Cloud Based Watch Faces, Black, Free Size (Black)',
+    rating: 41,
+    price: 1299,
+    mrp: 8999,
+    discount: 86,
+    ltd: false,
+    dod: false,
+    ed: false
+},{
+    image: '../images/products/watch24.webp',
+    name: 'Pebble Newly Launched Cruise 1.96" Infinite Display, 320 * 386 High-Resolution BT Calling Smartwatch, Rotating Crown, AI Health Sensors & Voice Assistant, 125+Sports Mode, 100+Watch Faces - Jet Black',
+    rating: 45,
+    price: 2649,
+    mrp: 7499,
+    discount: 65,
+    ltd: false,
+    dod: false,
+    ed: false
+}];
